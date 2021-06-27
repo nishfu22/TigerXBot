7081419438d3f19ae0eae560d604ed5144a7daa8
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-28 00:32:42 +0700

    Update quotly.py

--- quotly.py
+++ quotly.py
@@ -1,12 +1,12 @@
 #
-# Ultroid - UserBot
+# PetercordPanda - UserBot
 #
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
 """
-✘ Commands Available -
+💐 Commands Available -
 
 • `{i}quotly | {i}qbot <colour name/code><replying a message>`
     send stickers to current chat with QuotlyBot.
@@ -25,7 +25,7 @@ from . import *
 ERR = "`Can you kindly disable your forward privacy settings for good?`"
 
 
-@ultroid_cmd(pattern="(quotly|qbot) ?(.*)")
+@ilhammansiz_cmd(pattern="(quotly|qbot) ?(.*)")
 async def _(event):
     if not event.reply_to_msg_id:
         return await eor(event, "```Reply to any user message.```")
@@ -34,28 +34,28 @@ async def _(event):
     reply_message.sender
     ac = await eor(event, "```Making a Quote```")
     col = event.pattern_match.group(2)
-    async with ultroid_bot.conversation(chat) as conv:
+    async with petercordpanda_bot.conversation(chat) as conv:
         try:
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=1031952739)
             )
-            er = await ultroid_bot.forward_messages(chat, reply_message)
+            er = await petercordpanda_bot.forward_messages(chat, reply_message)
             if not len(col) == 0:  # Bad way
                 await asyncio.sleep(3)
                 await er.reply(f"/q {col}")
             response = await response
-            await ultroid_bot.send_read_acknowledge(chat)
+            await petercordpanda_bot.send_read_acknowledge(chat)
         except YouBlockedUserError:
             return await event.reply("```Please unblock @QuotLyBot and try again```")
         if response.text.startswith("Hi!"):
             await eor(event, ERR)
         else:
             await ac.delete()
-            await ultroid_bot.send_message(event.chat_id, response.message)
+            await petercordpanda_bot.send_message(event.chat_id, response.message)
 
 
 # Oringinal Source from Nicegrill: https://github.com/erenmetesar/NiceGrill/
-# Ported to Ultroid
+# Ported to petercordpanda
 
 
 import json
@@ -476,7 +476,7 @@ async def replied_user(draw, tot, text, maxlength, title):
             space += textfont.getsize(letter)[0]
 
 
-@ultroid_cmd(pattern="q$")
+@ilhammansiz_cmd(pattern="q$")
 async def _(event):
     reply = await event.get_reply_message()
     msg = reply.message
