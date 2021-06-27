c7c1a6a4b41e01166c6024d65d24643578bc214d
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-28 00:10:46 +0700

    Update covid.py

--- covid.py
+++ covid.py
@@ -1,12 +1,6 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
 """
-✘ Commands Available
+💐 Commands Available
 • `{i}covid country name`
     Gets the Covid-19 Status of a given Country.
 """
@@ -16,7 +10,7 @@ from covid import Covid
 from . import *
 
 
-@ultroid_cmd(pattern="covid")
+@ilhammansiz_cmd(pattern="covid")
 async def coronish(event):
     covid = Covid()
     text = event.text
